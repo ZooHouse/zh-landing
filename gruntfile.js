4b997a3af7fc185df3c13bb5bce18e5088ec0ce5
Clement Hoang <clement.hoang24@gmail.com>
2016-08-13 12:10:32 -0700

    :nail_care: Clean up gruntfile and package.json

--- gruntfile.js
+++ gruntfile.js
@@ -15,51 +15,13 @@ module.exports = function(grunt) {
 				}
 			}
 		},
-		shell: {
-	        git_add: {
-	            command: 'git add -A'
-	        },
-	        git_commit: {
-	            command: 'git commit -m "<%= pkg.name %> - <%= pkg.lastComment %>"'
-	        },
-	        git_push: {
-	            options: {
-	                stdout: true
-	            },
-	            command: 'git push'
-	        },
-	        shutdown: {
-	            command: 'shutdown /p'
-	        }
-		},
-		'ftp-deploy': {
-		  build: {
-		    auth: {
-		      host: 'dyaa.me',
-		      port: 21,
-		      authKey: 'key1'
-		    },
-		    src: '.',
-		    dest: '#',
-		    exclusions: ['.ftppass', '.git','node_modules','gruntfile.js','package.json','app/sass/**.scss']
-		  }
-		},
-		backup: {
-		    root_backup: {
-		      	src: '.',
-		      	dest: '../<%= pkg.name %>.tgz'
-		    },
-		},
-	    watch: {
-	    	options: { livereload: true },
-	      	sass: {
-	        	files: ['app/sass/styles.scss'],
-	        	tasks: ['newer:sass:dist'],
-	      	}
-	    },
-	    bumpup: {
-	        file: 'package.json'
-	    },
+    watch: {
+    	options: { livereload: true },
+      	sass: {
+        	files: ['app/sass/styles.scss'],
+        	tasks: ['newer:sass:dist'],
+      	}
+    },
 		connect: {
 			server: {
 				options: {
@@ -74,17 +36,8 @@ module.exports = function(grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-
 	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-newer');
-	grunt.loadNpmTasks('grunt-shell');
-	grunt.loadNpmTasks('grunt-backup');
-	grunt.loadNpmTasks('grunt-ftp-deploy');
-	grunt.loadNpmTasks('grunt-bumpup');
 
 	grunt.task.registerTask('default', ['sass', 'connect', 'watch']);
-	grunt.task.registerTask('git', ['bumpup:patch','shell:git_add','shell:git_commit','shell:git_push']);
-	grunt.task.registerTask('ftp', ['ftp-deploy']);
-	grunt.task.registerTask('goodbye', ['shell:shutdown']);
-
 };
